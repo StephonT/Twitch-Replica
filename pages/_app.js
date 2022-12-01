dcb1b0242e7fa6f0cf20437b159fb5ba4fccbc8b
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-12-01 18:11:45 -0500

    installed nextauth.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import {SessionProvider} from 'next-auth/react'
 
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ session, Component, pageProps }) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
