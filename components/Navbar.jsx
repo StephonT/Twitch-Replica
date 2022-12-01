c722376e63dd374a567393dd0bb2f49370dbd1d4
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-12-01 17:55:26 -0500

    finished Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,26 +1,33 @@
 import Link from "next/link";
 import Image from "next/image";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Logo from "../public/assets/images/logo.png";
 import { Menu, Transition } from "@headlessui/react";
-import {BsThreeDotsVertical} from 'react-icons/bs'
+import { BsSearch, BsThreeDotsVertical, BsPerson } from "react-icons/bs";
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 // function to join classNames
 function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 function Navbar() {
+const [nav, setNav] = useState(false)
+
+const handleNav = () => {
+    setNav(!nav)
+}
+
   return (
     <div className="fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0e0e10] mb-[2px] z-10">
       {/* Left Side */}
-      <div className="fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0e0e10] mb-[2px] z-10">
+      <div className="flex grow items-center justify-start">
         {/* Cannot wrap link around another functional component so in this case, I used an "a" tag. I also imported Image from Next.js and used legacyBehavior in Link */}
         <Link href="/" legacyBehavior>
           <a className="flex">
             <Image
               src={Logo}
-              alt="Twitch Logo"
+              alt="/"
               width="36"
               height="36"
               className="cursor-pointer z-10"
@@ -28,8 +35,6 @@ function Navbar() {
           </a>
         </Link>
         <p className="p-4 font-bold hover:text-[#9147ff]">Browse</p>
-
-        {/* Headless U/I */}
         <div className="p-4">
           <Menu as="div" className="relative text-left">
             <div className="flex">
@@ -101,8 +106,58 @@ function Navbar() {
         </div>
       </div>
 
-      {/* Middle */}
+      {/* Middle (Search) */}
+      <div className="hidden md:flex grow-[2] items-center justify-center">
+        <div className="bg-gray-500 text-white flex justify-between items-center max-w-[400px] w-full m-auto p-2 rounded-2xl">
+          <div>
+            <input
+              type="text"
+              className="bg-transparent border-none text-white focus:outline-none"
+              placeholder="Search"
+            />
+          </div>
+          <div>
+            <BsSearch />
+          </div>
+        </div>
+      </div>
+
       {/* Right Side */}
+      <div className="hidden md:flex grow items-center justify-end">
+        <div className="flex items-center">
+          <Link href="/">
+            <button className="px-4 py-[6px] rounded-lg font-bold bg-[#9147ff] mr-2">
+              Account
+            </button>
+          </Link>
+          <BsPerson size={30} />
+        </div>
+      </div>
+
+      {/* Hamburger Menu */}
+
+      <div onClick={handleNav} className="block md:hidden z-10 cursor-pointer">
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+      </div>
+
+      {/* Mobile Menu */}
+
+      <div className={nav ? "md:hidden fixed top-0 left-0 w-full h-screen bg-[#0e0e10] flex justify-center items-center ease-in duration-300" : "md:hidden fixed top-[-100%] left-0 w-full h-screen bg-[#0e0e10] flex justify-center items-center ease-in duration-300"}>
+        <ul className="text-center">
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Live Channels</Link>
+          </li>
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Top Categories</Link>
+          </li>
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Account</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
