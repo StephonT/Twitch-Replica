81b87ee0fe2e79fbf0c6d8fa5758361a8d653107
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-12-02 16:33:28 -0500

    embedded video and added media query styling

---
+++ components/IconBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Headphones from '../public/assets/images/headphones.png'
+import Mic from '../public/assets/images/mic.png'
+import Paint from '../public/assets/images/paint.png'
+import Remote from '../public/assets/images/remote.png'
+import Trophy from '../public/assets/images/trophy.png'
+
+
+const IconBar = () => {
+  return (
+    <div>IconBar</div>
+  )
+}
+
+export default IconBar
