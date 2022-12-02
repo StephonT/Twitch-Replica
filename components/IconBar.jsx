947b6e7d2d2119d5fdb303582992ac96135f9ff9
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-12-02 16:57:46 -0500

    added icon bars

--- components/IconBar.jsx
+++ components/IconBar.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 import Headphones from '../public/assets/images/headphones.png'
 import Mic from '../public/assets/images/mic.png'
@@ -8,7 +9,32 @@ import Trophy from '../public/assets/images/trophy.png'
 
 const IconBar = () => {
   return (
-    <div>IconBar</div>
+    <div className='p-2 md:p-8'>
+        {/* container */}
+        <div className='grid sm:grid-cols-3 lg:grid-cols-5 gap-4 border-t border-gray-700 py-8'>
+          {/* Grid Item */}
+          <div className='w-full h-[50px] bg-[#9147ff] flex justify-between items-center px-4 rounded'>
+            <p className="text-2xl font-bold ">Games</p>
+            <Image src={Remote} alt='remote logo'  />
+          </div>
+          <div className='w-full h-[50px] bg-[#9147ff] flex justify-between items-center px-4 rounded'>
+            <p className="text-2xl font-bold ">IRL</p>
+            <Image src={Mic} alt='remote logo'  />
+          </div>
+          <div className='w-full h-[50px] bg-[#9147ff] flex justify-between items-center px-4 rounded'>
+            <p className="text-2xl font-bold ">Music</p>
+            <Image src={Headphones} alt='remote logo'  />
+          </div>
+          <div className='w-full h-[50px] bg-[#9147ff] flex justify-between items-center px-4 rounded'>
+            <p className="text-2xl font-bold ">Esports</p>
+            <Image src={Trophy} alt='remote logo'  />
+          </div>
+          <div className='w-full h-[50px] bg-[#9147ff] flex justify-between items-center px-4 rounded'>
+            <p className="text-2xl font-bold ">Creative</p>
+            <Image src={Paint} alt='remote logo'  />
+          </div>
+        </div>
+    </div>
   )
 }
 
