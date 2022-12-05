a35bd2453e207b5f949e31f1cb88ac7f0df927d7
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-12-05 17:31:14 -0500

    changed function arguments

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "../styles/globals.css";
 import {SessionProvider} from 'next-auth/react'
 
 
-function MyApp({ session, Component, pageProps }) {
+function MyApp({ Component, pageProps: {session, ...pageProps} }) {
   return (
     <SessionProvider session={session}>
       <Navbar />
