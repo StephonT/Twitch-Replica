a0a0b932cc6b6981123d49ba4553d048a8855b8a
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-12-08 08:23:53 -0500

    prettier command

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
